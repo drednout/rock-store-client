51731ed21bab963b2ce8a21f7db17d1a5a02e03b
Alex Ramanau <drednout.by@gmail.com>
2025-04-13 11:07:47 +0200

    Add basic implementation of rocks finding

--- cmd/find.go
+++ cmd/find.go
@@ -1,22 +1,45 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
 
+type RockFindResponse struct {
+	Results []RockFindResult `json:"results"`
+}
+type RockFindResult struct {
+	Name             string   `json:"name"`
+	PackageId        string   `json:"package_id"`
+	RockFindMetadata Metadata `json:"metadata"`
+}
+type RockFindMetadata struct {
+	Description string    `json:"description"`
+	Publisher   Publisher `json:"publisher"`
+	Summary     string    `json:"summary"`
+}
+
 func rock_find(cmd *cobra.Command, args []string) {
+	fields := []string{
+		"summary",
+		"description",
+		"publisher",
+	}
 	params := url.Values{}
 	params.Add("q", args[0])
+	params.Add("fields", strings.Join(fields, ","))
 	encodedParams := params.Encode()
-    rock_find_url := fmt.Sprintf("%s/v2/rocks/find?%s",
+	rock_find_url := fmt.Sprintf("%s/v2/rocks/find?%s",
 		storeUrl, encodedParams)
-    fmt.Printf("SCAFFOLD: rock_find_url is %s\n", rock_find_url)
 	res, err := http.Get(rock_find_url)
 	if err != nil {
 		log.Fatal(err)
@@ -30,5 +53,24 @@ func rock_find(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", body)
+	var resp RockFindResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.Debug)
+	fmt.Fprintln(w, "Name\tPublisher\tSummary\t")
+	sep := strings.Repeat("-", 25)
+	fmt.Fprintf(w, "%s\t%s\t%s\t\n", sep, sep, sep)
+
+	for _, item := range resp.Results {
+		fmt.Fprintf(w, "%s\t%s (%s)\t%s\t\n",
+			item.Name,
+			item.RockFindMetadata.Publisher.DisplayName,
+			item.RockFindMetadata.Publisher.Username,
+			item.RockFindMetadata.Summary,
+		)
+	}
+	w.Flush()
 }
